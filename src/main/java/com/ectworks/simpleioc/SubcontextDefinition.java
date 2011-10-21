4e70efd1f0e3327e935e18fe439208eeadc530c9
mschaef <schaeffer.michael.a@gmail.com>
2011-10-20 22:09:14 -0400

    Add a TODO describing an ambiguity relating to exported names from subcontexts.

--- src/main/java/com/ectworks/simpleioc/SubcontextDefinition.java
+++ src/main/java/com/ectworks/simpleioc/SubcontextDefinition.java
@@ -32,6 +32,23 @@ class SubcontextDefinition extends Definition {
 
     public <T> T getInstance(Class<T> klass)
     {
+        // TODO: getInstance pulls the definition off the bindings
+        // list, rather than the exports list. This can produce
+        // unexpected results in the following scenario:
+        //
+        // class A;
+        // class B extends A;
+        //
+        // ctx.define(B);
+        // ctx.define(A);
+        // ctx.export(B);
+        //
+        // At this point, the definition of A shadows the definition
+        // of B. However, B has been exported and A has not.
+        //
+        // What should the context produce as a subcontext, when it's
+        // requested to produce A? (I think it should respond with B.)
+
         return (T)subcontext.getInstance(klass);
     }
 }
