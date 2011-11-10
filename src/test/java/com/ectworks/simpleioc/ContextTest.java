a2ce44468ddd810069593d44cdd70e3636b01908
mschaef <schaeffer.michael.a@gmail.com>
2011-11-09 23:11:41 -0500

    Add Transitive dependency and resolution order test cases.

--- src/test/java/com/ectworks/simpleioc/ContextTest.java
+++ src/test/java/com/ectworks/simpleioc/ContextTest.java
@@ -13,6 +13,8 @@ public class ContextTest {
         ctx = new Context("test-context");
     }
 
+    // Test Classes
+
     static class TestClass1 { public TestClass1() { }; }
     static class TestClass2 { public TestClass2() { }; }
 
@@ -26,8 +28,23 @@ public class ContextTest {
         }
     }
 
-    // Context Self
+    static class TransitiveTestClass {
+        public CompositeTestClass ctc = null;
+        public TestClass1 tc1 = null;
 
+        public TransitiveTestClass(CompositeTestClass ctc,
+                                   TestClass1 tc1)
+        {
+            this.ctc = ctc;
+            this.tc1 = tc1;
+        }
+    }
+
+    static class ClassBase {};
+    static class ClassSub extends ClassBase {};
+    static class ClassSubSub extends ClassSub {};
+
+    // Context Self
     @Test
     public void contextReturnsSelf()
     {
@@ -37,7 +54,6 @@ public class ContextTest {
     }
 
     // Instance
-
     @Test
     public void contextReturnsInstance()
     {
@@ -196,4 +212,136 @@ public class ContextTest {
         assertSame(ctc.tc1, ctx.getInstance(TestClass1.class));
         assertSame(ctc.tc2, ctx.getInstance(TestClass2.class));
     }
+
+    // Prototype
+    @Test
+    public void contextReturnsPrototype()
+    {
+        ctx.definePrototype(TestClass1.class);
+
+        Object inst = ctx.getInstance(TestClass1.class);
+
+        assertTrue(inst instanceof TestClass1);
+    }
+
+    @Test
+    public void contextReturnsTwoPrototype()
+    {
+        ctx.definePrototype(TestClass1.class);
+
+        Object inst = ctx.getInstance(TestClass1.class);
+        Object inst2 = ctx.getInstance(TestClass1.class);
+
+        assertTrue(inst instanceof TestClass1);
+        assertTrue(inst2 instanceof TestClass1);
+
+        assertNotSame(inst, inst2);
+    }
+
+    @Test
+    public void contextAllowsMultiplePrototypeTypes()
+    {
+        ctx.definePrototype(TestClass1.class);
+        ctx.definePrototype(TestClass2.class);
+
+        Object inst1 = ctx.getInstance(TestClass1.class);
+        Object inst2 = ctx.getInstance(TestClass2.class);
+
+        assertTrue(inst1 instanceof TestClass1);
+        assertTrue(inst2 instanceof TestClass2);
+
+        Object inst1a = ctx.getInstance(TestClass1.class);
+        Object inst2a = ctx.getInstance(TestClass2.class);
+
+        assertNotSame(inst1, inst1a);
+        assertNotSame(inst2, inst2a);
+
+        assertTrue(inst1a instanceof TestClass1);
+        assertTrue(inst2a instanceof TestClass2);
+    }
+
+    @Test
+    public void contextResolvesPrototypeDependencies()
+    {
+        ctx.definePrototype(TestClass1.class);
+        ctx.definePrototype(TestClass2.class);
+        ctx.definePrototype(CompositeTestClass.class);
+
+        CompositeTestClass ctc =
+            ctx.getInstance(CompositeTestClass.class);
+
+        TestClass1 inst1 = ctx.getInstance(TestClass1.class);
+        TestClass2 inst2 = ctx.getInstance(TestClass2.class);
+
+        assertNotSame(ctc.tc1, inst1);
+        assertNotSame(ctc.tc2, inst2);
+    }
+
+    // Dependency errors
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void contextRequiresSingletonDependencies1()
+    {
+        ctx.defineSingleton(CompositeTestClass.class);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void contextRequiresPrototypeDependencies1()
+    {
+        ctx.definePrototype(CompositeTestClass.class);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void contextRequiresSingletonDependencies2()
+    {
+        ctx.defineSingleton(TestClass1.class);
+        ctx.defineSingleton(CompositeTestClass.class);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void contextRequiresPrototypeDependencies2()
+    {
+        ctx.defineSingleton(TestClass1.class);
+        ctx.definePrototype(CompositeTestClass.class);
+    }
+
+    // Transitive Dependencies
+    public void contextResolvesTransitiveSingletonDependencies()
+    {
+        ctx.defineSingleton(TestClass1.class);
+        ctx.defineSingleton(TestClass2.class);
+        ctx.defineSingleton(CompositeTestClass.class);
+        ctx.defineSingleton(TransitiveTestClass.class);
+
+        TransitiveTestClass ttc = ctx.getInstance(TransitiveTestClass.class);
+        CompositeTestClass ctc  = ctx.getInstance(CompositeTestClass.class);
+        TestClass1 inst1        = ctx.getInstance(TestClass1.class);
+        TestClass2 inst2        = ctx.getInstance(TestClass2.class);
+
+        assertSame(ttc.ctc, ctc);
+        assertSame(ttc.tc1, inst1);
+        assertSame(ctc.tc1, inst1);
+        assertSame(ctc.tc2, inst2);
+    }
+
+
+    // Subclass name resolution
+    public void contextResolvesToFirstMatch1()
+    {
+        ctx.defineSingleton(ClassBase.class);
+        ctx.defineSingleton(ClassSub.class);
+        ctx.defineSingleton(ClassSubSub.class);
+
+        assertTrue(ctx.getInstance(ClassBase.class) instanceof ClassSubSub);
+    }
+
+    public void contextResolvesToFirstMatch2()
+    {
+        ctx.defineSingleton(ClassSubSub.class);
+        ctx.defineSingleton(ClassSub.class);
+        ctx.defineSingleton(ClassBase.class);
+
+        assertTrue(ctx.getInstance(ClassBase.class) instanceof ClassBase);
+    }
 }
+
