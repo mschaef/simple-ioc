ffd686e1ff359aecab2bebb3574f9e67a5f00b08
mschaef <schaeffer.michael.a@gmail.com>
2011-11-09 21:35:04 -0500

    Start adding context tests.

--- src/test/java/com/ectworks/simpleioc/ContextTest.java
+++ src/test/java/com/ectworks/simpleioc/ContextTest.java
@@ -1,12 +1,37 @@
 package com.ectworks.simpleioc;
 
 import org.testng.annotations.*;
+import static org.testng.Assert.*;
 
 public class ContextTest {
 
-    @Test
-    public void test1()
+    Context ctx = null;
+
+    @BeforeTest
+    void setupTest()
     {
-        System.out.println("Hello");
+        ctx = new Context("test-context");
+    }
+
+    static class TestClass {}
+
+    @Test
+    public void contextReturnsSelf()
+    {
+        Context ctx2 = ctx.getInstance(Context.class);
+
+        assertSame(ctx, ctx2);
+    }
+
+    @Test
+    public void contextReturnsInstance()
+    {
+        TestClass inst = new TestClass();
+
+        ctx.defineInstance(inst);
+
+        TestClass inst2 = ctx.getInstance(TestClass.class);
+
+        assertSame(inst, inst2);
     }
 }
