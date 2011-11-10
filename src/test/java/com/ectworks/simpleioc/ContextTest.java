4d896a67f5dbf0a1e756aea8385118e74abf6334
mschaef <schaeffer.michael.a@gmail.com>
2011-11-09 21:15:30 -0500

    Add a stub class for Context unit tests.

---
+++ src/test/java/com/ectworks/simpleioc/ContextTest.java
@@ -0,0 +1,12 @@
+package com.ectworks.simpleioc;
+
+import org.testng.annotations.*;
+
+public class ContextTest {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("Hello");
+    }
+}
